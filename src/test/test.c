66d761f181403bd0bd7588cd6638419068f92760
Daniel Nehrig <daniel.nehrig@redteclab.com>
2018-05-30 11:54:02 +0200

    done tests

--- src/test/test.c
+++ src/test/test.c
@@ -2,19 +2,33 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-  int size = 3;
+  int size = 0;
+
+  // Get Array Size square
+  do {
+    printf("Enter size between 3 - 20 : ");
+    scanf(" %d", &size);
+  } while (size < 3 || size > 20);
+
+  // Create Array
   char **arr;
-  arr = malloc(size * sizeof(char));
+  arr = malloc(size * sizeof(char*));
+
+  // Create 2D Array
   for (int i = 0; i < size; i++) {
     arr[i] = malloc(size * sizeof(char));
   }
 
+  // Init Array with dash
   for (int i = 0; i < size; i++) {
     for (int k = 0; k < size; k++) {
-      *(arr + i * size + k) = '-';
+      arr[i][k] = '-';
     }
   }
 
+  arr[1][2] = '+';
+
+  // Print Field
   for (int i = 0; i < size; i++) {
     for (int k = 0; k < size; k++) {
       printf("[ %c ] ", arr[i][k]);
